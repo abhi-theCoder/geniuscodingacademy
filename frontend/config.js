acea3e45fb114566a31a3c8bfa426317d7e5f514
abhi-theCoder <abhishekkumarmahto2005@gmail.com>
2026-06-21 02:56:47 +0530

    Added reset pass in admin's dashboard, Internship/Course certificate with QR and verification link , Offer letter creation with QR and verification link

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     // UPDATE THIS URL AFTER DEPLOYING YOUR BACKEND (e.g., https://your-backend.onrender.com)
-    BACKEND_URL: 'https://backend-geniusca.online'
-    // BACKEND_URL:'http://127.0.0.1:3000'
+    // BACKEND_URL: 'https://backend-geniusca.online'
+    BACKEND_URL:'http://127.0.0.1:3001'
 };
 
 // Make it available globally
